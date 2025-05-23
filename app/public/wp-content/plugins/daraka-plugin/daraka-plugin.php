8e3e8df924027e6daddd325d462105ec684c4b6b
HamzaSANMO <hamzasanmo@gmail.com>
2025-05-23 14:52:26 +0000

    content

---
+++ app/public/wp-content/plugins/daraka-plugin/daraka-plugin.php
@@ -0,0 +1,7 @@
+<?php
+/**
+* Plugin Name: daraka-plugin
+* Description: Ajoutez une citation aléatoire à chaque rechargement de votre site WordPress.
+* Version:     1.0.0
+* Author:      Team Daraka
+*/
